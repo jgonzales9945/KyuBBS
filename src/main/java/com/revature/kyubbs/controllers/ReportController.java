6f1f0d69e82f870c3a1b55ccbc51ca74eb419978
Joseph Gonzales <35093076+jgonzales9945@users.noreply.github.com>
2018-05-06 15:05:29 -0400

    Report, UserType, BannedIP controllers

---
+++ src/main/java/com/revature/kyubbs/controllers/ReportController.java
@@ -0,0 +1,69 @@
+package com.revature.kyubbs.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.kyubbs.models.Report;
+import com.revature.kyubbs.services.ReportService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/report")
+public class ReportController {
+	
+	@Autowired
+	ReportService service;
+	
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Report> findAllReports() {
+		return service.findAllReports();
+	}
+	
+	@GetMapping(value="/{ip}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Report findReportByIp(@PathVariable("ip") String ip) {
+		return service.findReportByIp(ip);
+	}
+	
+	@GetMapping(value="/{postid}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Report findReportByPostId(@PathVariable("postid") int id) {
+		return service.findReportByPostId(id);
+	}
+	
+	@GetMapping(value="/{threadid}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Report findReportByThreadId(@PathVariable("threadid") int id) {
+		return service.findReportByThreadId(id);
+	}
+	
+	@PostMapping(produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public Report addReport(@Valid @RequestBody Report r) {
+		return service.addReport(r);
+	}
+	
+	@GetMapping(value="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Report findReportById(@PathVariable("id") Long id) {
+		return service.findReportById(id);
+	}
+	
+	@PatchMapping(produces="application/json", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public Report updateReport(@Valid @RequestBody Report r) {
+		return service.updateReportById(r);
+	}
+	
+	@DeleteMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void deleteReport(@Valid @RequestBody Report r) {
+		service.deleteReportById(r);
+	}
+}
---
+++ src/main/java/com/revature/kyubbs/controllers/UserTypeController.java
@@ -0,0 +1,60 @@
+package com.revature.kyubbs.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.kyubbs.models.UserType;
+import com.revature.kyubbs.services.UserTypeService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/usertype")
+public class UserTypeController {
+	
+	@Autowired
+	UserTypeService service;
+	
+	@GetMapping(value="/{account}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public UserType findUserTypeByUserAccount(@PathVariable("account") String userAccount) {
+		return service.findUserTypeByUserAccount(userAccount);
+	}
+
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<UserType> findAllUserType() {
+		return service.findAllUserType();
+	}
+	
+	@PostMapping(produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public UserType addUserType( @Valid @RequestBody UserType a) {
+		return service.addUserType(a);
+	}
+	
+	@DeleteMapping(value="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public UserType findUserTypeById( @PathVariable("id") Long id) {
+		return service.findUserTypeById(id);
+	}
+	
+	@PatchMapping(produces="application/json", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public UserType updateUserType(@Valid @RequestBody UserType a) {
+		return service.updateUserTypeById(a);
+	}
+	
+	@GetMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void deleteUserType(@Valid @RequestBody UserType a) {
+		service.deleteUserTypeById(a);
+	}
+
+}
