b788a38b0fde4979105790185502f4bd725b20a8
Noah Dering <noahdering@gmail.com>
2018-05-05 21:07:39 -0400

    Fixed corrupted AuthUser.java file

--- src/main/java/com/revature/kyubbs/models/AuthUser.java
+++ src/main/java/com/revature/kyubbs/models/AuthUser.java
@@ -1,6 +1,5 @@
 package com.revature.kyubbs.models;
 
-public class AuthUser {
 import java.io.Serializable;
 
 import javax.persistence.Column;
@@ -34,8 +33,89 @@ public class AuthUser implements Serializable {
 	private Long userTypeId;
 
 	public AuthUser() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
+	public AuthUser(Long userId, @NotNull String username, @NotNull Long userTypeId) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.userTypeId = userTypeId;
+	}
+
+	public AuthUser(@NotNull String username, @NotNull Long userTypeId) {
+		super();
+		this.username = username;
+		this.userTypeId = userTypeId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Long getUserTypeId() {
+		return userTypeId;
+	}
+
+	public void setUserTypeId(Long userTypeId) {
+		this.userTypeId = userTypeId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		result = prime * result + ((userTypeId == null) ? 0 : userTypeId.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AuthUser other = (AuthUser) obj;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		if (userTypeId == null) {
+			if (other.userTypeId != null)
+				return false;
+		} else if (!userTypeId.equals(other.userTypeId))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "AuthUser [userId=" + userId + ", username=" + username + ", userTypeId=" + userTypeId + "]";
+	}
 }
->>>>>>> f56ff0bdcdb432613bb5f9ab7995fc3a38fd5f99
