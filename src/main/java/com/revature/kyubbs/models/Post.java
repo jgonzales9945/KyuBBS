30d2fc34deba5b4407ec5a72146f3460f1dc4c7d
jgonzales9945 <jgonzales9945@gmail.com>
2018-05-09 20:22:28 -0500

    fixed some post functions

--- src/main/java/com/revature/kyubbs/models/Post.java
+++ src/main/java/com/revature/kyubbs/models/Post.java
@@ -146,6 +146,71 @@ public class Post implements Serializable {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (authUserId ^ (authUserId >>> 32));
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + flag;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((ipAddress == null) ? 0 : ipAddress.hashCode());
+		result = prime * result + ((modifiedDate == null) ? 0 : modifiedDate.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+		result = prime * result + (int) (threadId ^ (threadId >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (authUserId != other.authUserId)
+			return false;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (flag != other.flag)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (ipAddress == null) {
+			if (other.ipAddress != null)
+				return false;
+		} else if (!ipAddress.equals(other.ipAddress))
+			return false;
+		if (modifiedDate == null) {
+			if (other.modifiedDate != null)
+				return false;
+		} else if (!modifiedDate.equals(other.modifiedDate))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+
+		} else if (!name.equals(other.name))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		if (threadId != other.threadId)
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Post [postId=" + id + ", name=" + name + ", content=" + content + ", startDate=" + startDate
 				+ ", modifiedDate=" + modifiedDate + ", flag=" + flag + ", ipAddress=" + ipAddress + ", userId="
